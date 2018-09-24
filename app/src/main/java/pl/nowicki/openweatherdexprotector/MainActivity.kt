9cb7e28863c37e70e2db13d6c241b898f414e4c8
Adam Nowicki <adam.novicki@gmail.com>
2018-09-24 11:56:56 +0200

    coroutines - handshake ex

--- app/src/main/java/pl/nowicki/openweatherdexprotector/MainActivity.kt
+++ app/src/main/java/pl/nowicki/openweatherdexprotector/MainActivity.kt
@@ -1,87 +1,41 @@
 package pl.nowicki.openweatherdexprotector
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
-import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
-import okhttp3.JavaNetCookieJar
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import pl.nowicki.openweatherdexprotector.model.WeatherRsp
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.net.CookieManager
-import java.net.CookiePolicy
-import java.util.concurrent.TimeUnit
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
-
-    val SERVER_URL = "https://api.openweathermap.org"
-
     lateinit var viewModel: WeatherViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val weatherApiService = getForecastService(this);
-
-        val cities: MutableList<String> = mutableListOf("Katowice", "London", "Ulcinj")
-        var cityCounter = 0
+        initModel()
 
         button.setOnClickListener {
-            val city = cities.get(++cityCounter % 3)
-
-            val weatherCall = getForecast(weatherApiService, city)
-            weatherCall.enqueue(object : Callback<WeatherRsp> {
-                override fun onFailure(call: Call<WeatherRsp>?, t: Throwable?) {
-                    tv.text = "FAIL"
-                }
-
-                override fun onResponse(call: Call<WeatherRsp>?, response: Response<WeatherRsp>?) {
-                    val sb = StringBuilder(response?.body()?.city!!.name.toString())
-                    sb.append(" ")
-                    sb.append(response.body()?.list!![0].main.temp.toString())
-                    tv.text = sb.toString()
-                }
-            })
+            viewModel.action()
         }
     }
 
     fun initModel() {
+        Timber.d("initModel")
         viewModel = ViewModelProviders.of(this).get(WeatherViewModel::class.java)
+        viewModel.city.observe {
+
+        }
+        viewModel.temperature.observe {
+            tv.text = it.toString()
+        }
     }
 
-    private fun getForecastService(context: Context): WeatherApiService {
-
-        val cookieManager = CookieManager()
-        cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL)
-
-        val okBuilder = OkHttpClient.Builder()
-        okBuilder.cookieJar(JavaNetCookieJar(cookieManager))
-
-        val logging = HttpLoggingInterceptor()
-        logging.level = HttpLoggingInterceptor.Level.BODY
-        okBuilder.addNetworkInterceptor(logging)
-        okBuilder.readTimeout(context.resources.getInteger(R.integer.read_timeout).toLong(), TimeUnit.MILLISECONDS)
-        okBuilder.connectTimeout(context.resources.getInteger(R.integer.connect_timeout).toLong(), TimeUnit.MILLISECONDS)
-
-        val retrofit = Retrofit.Builder()
-                .baseUrl(SERVER_URL)
-                .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
-                .client(okBuilder.build())
-                .build()
-
-        return retrofit.create(WeatherApiService::class.java)
-    }
-
-    private fun getForecast(weatherService: WeatherApiService, city: String): Call<WeatherRsp> {
-        return weatherService.forecast(city)
-    }
+    private fun <T> LiveData<T>.observe(observe: (T?) -> Unit) = observe(this@MainActivity, Observer {
+        Timber.d("onChanged $it")
+        observe(it)
+    })
 
 }
--- app/src/main/java/pl/nowicki/openweatherdexprotector/WeatherViewModel.kt
+++ app/src/main/java/pl/nowicki/openweatherdexprotector/WeatherViewModel.kt
@@ -2,15 +2,113 @@ package pl.nowicki.openweatherdexprotector
 
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import com.google.gson.GsonBuilder
 import kotlinx.coroutines.experimental.CoroutineScope
 import kotlinx.coroutines.experimental.Dispatchers
 import kotlinx.coroutines.experimental.android.Main
+import kotlinx.coroutines.experimental.channels.Channel
+import kotlinx.coroutines.experimental.channels.actor
+import okhttp3.JavaNetCookieJar
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import pl.nowicki.openweatherdexprotector.model.WeatherRsp
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
+import java.net.CookieManager
+import java.net.CookiePolicy
+import java.util.concurrent.TimeUnit
 import kotlin.coroutines.experimental.CoroutineContext
 
 class WeatherViewModel : ViewModel(), CoroutineScope {
+
+    val SERVER_URL = "https://api.openweathermap.org"
+
+    val cities: MutableList<String> = mutableListOf("Katowice", "London", "Ulcinj")
+    var cityCounter = 0
+
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main
 
-    private val mutableCity = MutableLiveData<String>().apply { value = "" }
-    private val mutableLoading = MutableLiveData<Boolean>().apply { value = false }
+    val city = MutableLiveData<String>().apply { value = "" }
+    val loading = MutableLiveData<Boolean>().apply { value = false }
+    val temperature = MutableLiveData<Double>().apply { value = 0.0 }
+
+    private val actor = actor<String>(Dispatchers.Main, Channel.CONFLATED) {
+        Timber.d("SelectCity")
+        for (selected in this) {
+            city.value = selected
+            loading.value = true
+            getTemperature(selected)
+            loading.value = false
+        }
+    }
+
+    init {
+        Timber.d("init")
+        //action()
+    }
+
+    fun action() = actor.offer(cities.get(++cityCounter % 3))
+
+    override fun onCleared() {
+        actor.close()
+    }
+
+    private suspend fun getTemperature(city: String) : Double {
+        Timber.d("getTemperature")
+        val weather = getDailyForecastsByCity(city)
+        return weather.list!![0].main.temp
+    }
+
+    private suspend fun getDailyForecastsByCity(city: String) : WeatherRsp {
+
+//        return suspendCoroutine {
+//            val weatherCall = getForecast(getForecastService(), city)
+//            weatherCall.enqueue(object : Callback<WeatherRsp> {
+//                override fun onFailure(call: Call<WeatherRsp>?, t: Throwable?) {
+//                    Timber.e("onFailure")
+//                }
+//
+//                override fun onResponse(call: Call<WeatherRsp>?, response: Response<WeatherRsp>?) {
+//                    val sb = StringBuilder(response?.body()?.city!!.name.toString())
+//                    sb.append(" ")
+//                    sb.append(response.body()?.list!![0].main.temp.toString())
+//                    Timber.e("sb: $sb")
+//                }
+//            })
+//
+//        }
+
+        return getForecast(getForecastService(), cities.get(++cityCounter % 3)).await()
+    }
+
+
+    private fun getForecastService(): WeatherApiService {
+
+        val cookieManager = CookieManager()
+        cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL)
+
+        val okBuilder = OkHttpClient.Builder()
+        okBuilder.cookieJar(JavaNetCookieJar(cookieManager))
+
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        okBuilder.addNetworkInterceptor(logging)
+        okBuilder.readTimeout(60, TimeUnit.MILLISECONDS)
+        okBuilder.connectTimeout(60, TimeUnit.MILLISECONDS)
+
+        val retrofit = Retrofit.Builder()
+                .baseUrl(SERVER_URL)
+                .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+                .client(okBuilder.build())
+                .build()
+
+        return retrofit.create(WeatherApiService::class.java)
+    }
+
+    private fun getForecast(weatherService: WeatherApiService, city: String): Call<WeatherRsp> {
+        return weatherService.forecast(city)
+    }
 }
